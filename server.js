5a36f0b6b00cf526a1bee3f676c41fbb9740c355
Yağmur ŞİMŞEK <yyagmursmsk.53@gmail.com>
2025-08-15 16:01:06 +0300

    add start script

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const multer = require("multer");
 const xlsx = require("xlsx");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
